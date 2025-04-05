8918e0e9dfde89befb94bf521dab31f83a713057
Konstantin Safronov <kostya.safronov@gmail.com>
2025-04-05 18:10:57 +0300

    Удален лишний файл

--- Selenium/code/test_login_vk.py
+++
@@ -1,8 +0,0 @@
-class TestLogin(BaseCase):
-    authorize = False  # отключаем авто-логин
-
-    def test_login(self, credentials):
-        page = LoginPage(self.driver)
-        main_page = page.login(credentials["username"], credentials["password"])
-
-        assert "feed" in self.driver.current_url
